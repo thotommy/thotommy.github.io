b071b78b3a63fabb38aac401167061082909e004
thotommy <thotommy94@gmail.com>
2020-05-02 01:37:27 -0400

    added parallax directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material/material.module';
+import { ParallaxDirective } from './directives/parallax.directive';
 
 @NgModule({
-  declarations: [],
+  declarations: [ParallaxDirective],
   imports: [CommonModule, FlexLayoutModule, MaterialModule],
   exports: [CommonModule, FlexLayoutModule, MaterialModule],
 })
