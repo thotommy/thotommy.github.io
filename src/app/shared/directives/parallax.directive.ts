4b06667ba6c183425eeedb5f85777bd554e10008
thotommy <thotommy94@gmail.com>
2020-05-02 23:25:19 -0400

    Created a parallax effect for the title page.

--- src/app/shared/directives/parallax.directive.ts
+++ src/app/shared/directives/parallax.directive.ts
@@ -1,8 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appParallax]',
 })
 export class ParallaxDirective {
-  constructor() {}
+  // tslint:disable-next-line: no-input-rename
+  @Input('ratio') parallaxRatio = 1;
+  initialTop = 0;
+
+  constructor(private eleRef: ElementRef) {
+    this.initialTop = this.eleRef.nativeElement.getBoundingClientRect().top;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event) {
+    this.eleRef.nativeElement.style.top =
+      this.initialTop - window.scrollY * this.parallaxRatio + 'px';
+  }
 }
