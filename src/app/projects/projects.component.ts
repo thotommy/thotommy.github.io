357790547e9f427d6f63f4dd9160eaf543fda3d4
thotommy <thotommy94@gmail.com>
2020-05-10 23:06:27 -0400

    Putting down the infrastructure for the skills, projects, and contact me page. Fixed some styling. Maybe need to fix some more.

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss'],
+})
+export class ProjectsComponent implements OnInit {
+  public items;
+
+  constructor() {
+    this.items = [];
+  }
+
+  ngOnInit(): void {}
+}
