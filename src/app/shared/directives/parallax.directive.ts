dd2e01ec375cb7827b11786ec424dae8c8af0e11
root <root@DESKTOP-PGSRLUT.localdomain>
2020-06-09 00:41:06 -0400

    Added some effects to the title splash page

--- src/app/shared/directives/parallax.directive.ts
+++ src/app/shared/directives/parallax.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, HostListener, ElementRef } from '@angular/core';
+import { Directive, Input, HostListener, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Directive({
   selector: '[appParallax]',
@@ -6,6 +6,7 @@ import { Directive, Input, HostListener, ElementRef } from '@angular/core';
 export class ParallaxDirective {
   // tslint:disable-next-line: no-input-rename
   @Input('ratio') parallaxRatio = 1;
+  @Output() startTransition = new EventEmitter<ParallaxTransition>();
   initialTop = 0;
 
   constructor(private eleRef: ElementRef) {
@@ -14,7 +15,24 @@ export class ParallaxDirective {
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event) {
-    this.eleRef.nativeElement.style.top =
-      this.initialTop - window.scrollY * this.parallaxRatio + 'px';
+    this.eleRef.nativeElement.style.top = this.initialTop - window.scrollY * this.parallaxRatio + 'px';
+
+    if (window.scrollY < 20) {
+      this.startTransition.emit({startTransition: false, element: this.eleRef.nativeElement} as ParallaxTransition);
+      return;
+    }
+    if (this.eleRef.nativeElement.getBoundingClientRect().top < -41) {
+      this.startTransition.emit({startTransition: true, element: this.eleRef.nativeElement} as ParallaxTransition);
+      return;
+    }
+    if (this.eleRef.nativeElement.getBoundingClientRect().top > -41 && window.scrollY > 20) {
+      this.startTransition.emit({startTransition: false, element: this.eleRef.nativeElement} as ParallaxTransition);
+      return;
+    }
   }
+}
+
+export class ParallaxTransition {
+  startTransition: boolean;
+  element: ElementRef;
 }
