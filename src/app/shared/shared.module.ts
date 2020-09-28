e9cafb7619507154b9d4a0a09fd9f7fcdf3c8e76
thotommy <thotommy94@gmail.com>
2020-09-28 00:19:14 -0400

    Removing animate.css. To bloatly for the things I am needing to do. Also consolidated the parallax directive to the portfolio component, since no one else will be using this directive.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material/material.module';
-import { ParallaxDirective } from './directives/parallax.directive';
 import { AnimatedTilesComponent } from './components/animated-tiles/animated-tiles.component';
 
 @NgModule({
-  declarations: [ParallaxDirective, AnimatedTilesComponent],
+  declarations: [AnimatedTilesComponent],
   imports: [CommonModule, FlexLayoutModule, MaterialModule],
-  exports: [CommonModule, FlexLayoutModule, MaterialModule, ParallaxDirective, AnimatedTilesComponent],
+  exports: [CommonModule, FlexLayoutModule, MaterialModule, AnimatedTilesComponent],
 })
 export class SharedModule {}
