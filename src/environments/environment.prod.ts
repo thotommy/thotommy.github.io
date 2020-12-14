738eeeb8b669a943ecaee0f15af3436a9047c441
thotommy <thotommy94@gmail.com>
2020-12-14 00:51:46 -0500

    Prepping got deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-require('dotenv').config();
 
 export const environment = {
-  production: true,
-  CONTACT_API_URL: process.env.CONTACT_API_URL,
+   production: true,
+   CONTACT_API_URL: "https://app.99inbound.com/api/e/DzDIKUH3",
 };
