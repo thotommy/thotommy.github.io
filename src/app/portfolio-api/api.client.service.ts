ae7dd8e46096dfeccb56b2df11553a356406bce7
thotommy <thotommy94@gmail.com>
2020-12-14 00:15:32 -0500

    Added a environment infrastructure in case for secrets in the future.

--- src/app/portfolio-api/api.client.service.ts
+++ src/app/portfolio-api/api.client.service.ts
@@ -9,12 +9,12 @@ export class ApiClientService {
   constructor(private http: HttpClient) {}
 
   get<T>(url: string): Observable<T> {
-    console.log(`Get call for ${url}`);
+    console.log(`Get api called`);
     return this.http.get<T>(url);
   }
 
   post<T>(url: string, data: any): Observable<T> {
-    console.log(`Post call for ${url}.`);
+    console.log(`Post api called`);
     const jsonData = JSON.stringify(data);
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
--- src/app/portfolio-api/portfolio-api.service.ts
+++ src/app/portfolio-api/portfolio-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ApiRoutes } from './portfolio-api';
 import { ApiClientService } from './api.client.service';
+import { SubmitContactRequest } from '../shared/models/models';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +10,10 @@ export class PortfolioApiService {
   constructor(private apiService: ApiClientService) {}
 
   retrieveGitRepos() {
-    return this.apiService.get<any>(
-      'https://api.github.com/users/thotommy/repos'
-    );
+    return this.apiService.get<any>('https://api.github.com/users/thotommy/repos');
+  }
+
+  submitContactInfo(req: SubmitContactRequest) {
+    return this.apiService.post<any>(environment.CONTACT_API_URL, req);
   }
 }
