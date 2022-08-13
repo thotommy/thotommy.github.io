7861a38b47b483b5e153ec08801f3f8abc384ef3
thotommy <thotommy94@gmail.com>
2022-08-12 23:15:55 -0400

    Ignore env, envprod, and modify env.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
+require('dotenv').config();
 
 export const environment = {
-   production: false,
-   CONTACT_API_URL: "https://app.99inbound.com/api/e/p8dhLiy4",
+  production: false,
+  CONTACT_API_URL: process.env.CONTACT_API_URL,
 };
