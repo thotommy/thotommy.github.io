14c3f512ab137b3e8d557c6320e7eace06cf7d96
thotommy <thotommy94@gmail.com>
2023-05-16 01:26:57 -0400

    Adding some more components to the first initialization of the project.

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function Index(): JSX.Element {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className="flex items-center justify-center gap-x-12">
+     
+    </div>
+  );
+}
