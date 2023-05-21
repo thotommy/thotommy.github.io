c4198a803fe39ae7f8fc0936b5fe461d40fa1508
thotommy <thotommy94@gmail.com>
2023-05-21 01:08:23 -0400

    Added title to the screen

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,8 +4,9 @@ export default function Index(): JSX.Element {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <div className="flex items-center justify-center gap-x-12">
-        fdsfsf
+    <div className="flex flex-col items-center justify-center gap-y-20 h-screen font-medium">
+        <h1 className="text-8xl">Tommy Ho</h1>
+        <h2 className="text-5xl">Software Engineer</h2>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+		  courier: ['Courier', 'monospace'],
+		  /* Add more font families if needed */
+		},
 	},
 	plugins: [],
 }
