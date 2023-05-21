ad233ff02b21d32852c6983d685cb6a7354c97fc
thotommy <thotommy94@gmail.com>
2023-05-21 01:28:10 -0400

    Added type writer effect on the title

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
+import { Typewriter } from 'react-simple-typewriter'
 
-export default function Index(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
+export default class Index extends Component {
+  // const [count, setCount] = useState<number>(0);
 
+    typeWrite() {
+    // const target2 = this.projectTypeWriter.nativeElement;
+    // console.log(target2);
+    // const writer2 = new Typewriter(target2, {
+    //   loop: false,
+    //   typeColor: 'white',
+    //   animateCursor: true,
+    //   cursorColor: 'white',
+    //   typeSpeed: 40,
+    //   deleteSpeed: 0,
+    // });
+
+    // this.isTitleVisible2 = writer2.type('cd Projects').queueClearText().type('Projects');
+  }
+  render(): JSX.Element {
   return (
-    <div className="flex flex-col items-center justify-center gap-y-20 h-screen font-medium">
+    <div className="flex flex-col items-center justify-center gap-y-10 h-screen font-medium">
         <h1 className="text-8xl">Tommy Ho</h1>
-        <h2 className="text-5xl">Software Engineer</h2>
+        <h2 className="text-5xl"><Typewriter
+            words={['Software Engineer', 'Full Stack Developer', 'Angular Developer']}
+            loop={0}
+            cursor
+            cursorStyle='|'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          /></h2>
     </div>
   );
+  }
 }
