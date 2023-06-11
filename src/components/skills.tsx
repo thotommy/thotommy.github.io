dffdf11cb08a692541b956d01d374e1db506df07
thotommy <thotommy94@gmail.com>
2023-06-11 12:26:27 -0400

    Working in the skills component

---
+++ src/components/skills.tsx
@@ -0,0 +1,17 @@
+// import react { Component, useEffect, useState } from 'react';
+// import { Typewriter } from 'react-simple-typewriter'
+
+// class SkillsComponent extends Component {
+//     const state: any
+//     // const [data, setData] = useState<Data | null>(null);
+
+//     render(): JSX.Element {
+//         const {data} = this.state
+//         return (
+//           <div className="flex flex-col items-center justify-center gap-y-10 h-screen font-medium mt-[-4rem]">
+//           </div>
+//         );
+//         }
+// }
+
+// export default SkillsComponent
