31cac6bacf0e0f74a26a58f572f6e83cba7dba88
thotommy <thotommy94@gmail.com>
2023-07-18 00:58:58 -0400

    formatting

--- src/components/mainTitleScreen.tsx
+++ src/components/mainTitleScreen.tsx
@@ -15,7 +15,6 @@ export const MainTitleScreen: React.FC = () => {
     setShowNavigation(false);
   };
 
-
   return (
     <div className="flex flex-col items-center justify-center gap-y-10 h-screen font-medium mt-[-4rem]">
         <h1 className="text-5xl md:text-5xl lg:text-6xl xl:text-8xl">Tommy Ho</h1>
@@ -29,37 +28,36 @@ export const MainTitleScreen: React.FC = () => {
             delaySpeed={1000}
           /></h2>
 
-    <div className="absolute mt-[39rem]">
-    {showBouncingDiv && (
-      <div className={`animate-bounce transition-opacity duration-500 ease-in-out ${
-            showBouncingDiv ? 'opacity-100' : 'opacity-0'
-          }`}id="arrow" onMouseEnter={handleBouncingDivMouseEnter} >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" fill="currentColor" className="w-10 h-10">
-          <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" clipRule="evenodd" />
-        </svg>
+      <div className="absolute mt-[39rem]">
+        {showBouncingDiv && (
+          <div className={`animate-bounce transition-opacity duration-500 ease-in-out ${
+                showBouncingDiv ? 'opacity-100' : 'opacity-0'
+              }`}id="arrow" onMouseEnter={handleBouncingDivMouseEnter} >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" fill="currentColor" className="w-10 h-10">
+              <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" clipRule="evenodd" />
+            </svg>
+          </div>
+        )}
+        {showNavigation && (
+          <div className={`transition-opacity duration-500 ease-in-out ${
+            showNavigation ? 'opacity-100' : 'opacity-0'
+          }`}
+          onAnimationEnd={() => {
+            if (!showNavigation) {
+              setShowNavigation(true);
+            }
+          }}
+          onMouseLeave={handleNavigationMouseLeave}>
+            <nav>
+              <ul className="flex space-x-4">
+                <li><a href="projects">Projects</a></li>
+                <li><a href="skills">Skills</a></li>
+                <li><a href="contact">Contact</a></li>
+              </ul>
+            </nav>
+          </div>
+        )}
       </div>
-      )}
-      {showNavigation && (
-      <div className={`transition-opacity duration-500 ease-in-out ${
-        showNavigation ? 'opacity-100' : 'opacity-0'
-      }`}
-      onAnimationEnd={() => {
-        if (!showNavigation) {
-          setShowNavigation(true);
-        }
-      }}
-      onMouseLeave={handleNavigationMouseLeave}>
-        <nav>
-          <ul className="flex space-x-4">
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Skills</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-      </div>
-            )}
-    </div>
-        
     </div>
   );
 }
