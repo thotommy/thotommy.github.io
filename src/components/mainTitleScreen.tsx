eb8470f963b6a5b8e7eb741d82f2e7c5446dbc24
thotommy <thotommy94@gmail.com>
2023-11-14 00:11:09 -0500

    Adjustments to styling to make it more terminal like. Will add glassmorphism later.

--- src/components/mainTitleScreen.tsx
+++ src/components/mainTitleScreen.tsx
@@ -17,7 +17,7 @@ export const MainTitleScreen: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-y-10 h-screen font-medium mt-[-7rem]">
+    <div className="flex flex-col items-center justify-center gap-y-10 h-[calc(100vh-110px)] font-medium mt-[-7rem]">
         <h1 className="text-5xl md:text-5xl lg:text-6xl xl:text-8xl">Tommy Ho</h1>
         <h2 className="sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl"><Typewriter
             words={['Software Engineer', 'Full Stack Developer', 'Angular Developer']}
