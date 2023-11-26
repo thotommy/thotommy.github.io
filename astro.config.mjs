66896284dea83a582f9b32d1d1e54a12ec238920
thotommy <thotommy94@gmail.com>
2023-11-26 17:12:07 -0500

    Setting up for github pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thotommy.github.io',
   integrations: [tailwind(), react()],
   experimental: {
 		viewTransitions: true
